2b4800b42d25806d8ac3e7c4f57132d8dc8cc89c
solahidris <emailsolah@gmail.com>
2025-02-19 22:49:49 +0800

    redo dashboard page to dialog instead

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -3,29 +3,60 @@ import Link from "next/link";
 
 const Dashboard = () => {
 
-  const goalWorkoutBoxCss = "flex flex-col bg-white"
+  const goalWorkoutBoxCss = "flex flex-col bg-white p-2 justify-center items-center drop-shadow-md rounded w-full aspect-square"
+  const goalNumberCss = "text-xl font-semibold"
+  const goalTextCss = "text-xs"
+
+  const workoutGoals = [
+    {number: "100x", text: "Pushup"},
+    {number: "100x", text: "Situps"},
+    {number: "100x", text: "Squats"},
+    {number: "10km", text: "Run"},
+  ]
+
+  const runningDistance = [
+    {number: "1", text: "km"},
+    {number: "2", text: "km"},
+    {number: "5", text: "km"},
+    {number: "10", text: "km"},
+  ]
 
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen bg-gradient-to-tr from-gray-800 via-gray-500 to-gray-800">
-      <div className="flex flex-col">
-        <p>Daily Workout Goal</p>
-        <div className="flex gap-2">
-          <div className={goalWorkoutBoxCss}>
-            <p>100x</p>
-            <p>Pushup</p>
-          </div>
-          <div className={goalWorkoutBoxCss}>
-            <p>100x</p>
-            <p>Situps</p>
-          </div>
-          <div className={goalWorkoutBoxCss}>
-            <p>100x</p>
-            <p>Squats</p>
-          </div>
-          <div className={goalWorkoutBoxCss}>
-            <p>10km</p>
-            <p>Run</p>
-          </div>
+    <div className="flex flex-col items-center p-6 min-h-screen bg-gradient-to-tr from-gray-800 via-gray-500 to-gray-800">
+      
+      {/* <div className="flex flex-col bg-gray-50 rounded p-4 w-full mb-4">
+        <p className="font-semibold text-center mt-2 mb-4">How many km are you comfortable running?</p>
+        <div className="flex justify-center gap-2">
+          {runningDistance.map((goal,key)=>(
+            <button key={key} className={goalWorkoutBoxCss}>
+              <p className={goalNumberCss}>{goal.number}</p>
+              <p className={goalTextCss}>{goal.text}</p>
+            </button>
+          ))}
+        </div>
+      </div>
+       */}
+      <div className="flex flex-col bg-gray-50 rounded p-4 w-full">
+        <p className="font-semibold text-center mt-2 mb-4">Daily Workout Goal</p>
+        <div className="flex justify-center gap-2">
+          {workoutGoals.map((goal,key)=>(
+            <div key={key} className={goalWorkoutBoxCss}>
+              <p className={goalNumberCss}>{goal.number}</p>
+              <p className={goalTextCss}>{goal.text}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      
+      <div className="flex flex-col bg-gray-50 rounded p-4 w-full mt-4">
+        <p className="font-semibold text-center mt-2 mb-4">How many km are you comfortable running?</p>
+        <div className="flex justify-center gap-2">
+          {runningDistance.map((goal,key)=>(
+            <button key={key} className={goalWorkoutBoxCss}>
+              <p className={goalNumberCss}>{goal.number}</p>
+              <p className={goalTextCss}>{goal.text}</p>
+            </button>
+          ))}
         </div>
       </div>
       {/* <img
@@ -36,7 +67,7 @@ const Dashboard = () => {
         className="h-full w-full max-w-[200px] rounded-lg drop-shadow-md"
       /> */}
       <Link href="/dashboard" className="mt-4 w-full flex justify-center">
-        <Button className="w-full max-w-[200px]">Start</Button>
+        <Button className="w-full bg-blue-600 hover:bg-blue-700 font-bold drop-shadow-md">Start Quest</Button>
       </Link>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,35 @@
+// import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  // DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+// import { Input } from "@/components/ui/input";
+// import { Label } from "@/components/ui/label";
+import { useState } from "react";
+
 
 const Home = () => {
+
+  // const [playGame, setPlayGame] = useState(null); // im planning to make this a boolean. but i want the initial value to be null. how?
+  const [playGame, setPlayGame] = useState<boolean | null>(null); // Initialize as null, can be boolean later
+  // const [playerName, setPlayerName] = useState("");
+  const [playMusic, setPlayMusic] = useState(false);
+
+  const handlePlayMusic = () => {
+    setPlayMusic(true);
+    console.log("music played:", !playMusic);
+    const audio = new Audio("./audio/dark_aria.mp3");
+    audio.play();
+  }
+
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen bg-gradient-to-tr from-gray-800 via-gray-500 to-gray-800">
+    <div className="flex flex-col justify-center items-center min-h-screen bg-gradient-to-tr from-gray-800 via-gray-500 to-gray-800 bg-opacity-[10%]">
       <img
         src="./images/main.png"
         alt="mainimage"
@@ -11,9 +37,48 @@ const Home = () => {
         height={400}
         className="h-full w-full max-w-[200px] rounded-lg drop-shadow-md"
       />
-      <Link href="/dashboard" className="mt-4 w-full flex justify-center">
-        <Button className="w-full max-w-[200px]">Start</Button>
-      </Link>
+
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button onClick={handlePlayMusic}className="w-full max-w-[200px] mt-4 font-bold">Start Quest</Button>
+          </DialogTrigger>
+          <DialogContent className="max-w-[90vw] lg:max-w-md rounded-lg bg-black text-white p-4">
+            <DialogHeader>
+              <DialogTitle className="py-4">{playGame === null && " New Player"}</DialogTitle>
+              <DialogDescription className="text-white/80 mt-8">
+                {playGame === null ? 
+                  <div>You have acquired the qualifications to be a <span className="font-bold text-white">Player</span>. Your fitness goals will die in the abyst if you choose not to accept.  <span className="font-bold text-white">Will you accept?</span></div>
+                : 
+                  <div className="font-bold text-4xl pb-8">GAME OVER</div>
+                }
+              </DialogDescription>
+            </DialogHeader>
+            {playGame === null &&
+              <div className="grid grid-cols-2 gap-4 py-4">
+                  <Button onClick={()=>setPlayGame(true)} className="bg-blue-600 hover:bg-blue-700 font-bold">Yes</Button>
+                  <Button onClick={()=>setPlayGame(false)} className="bg-red-600 hover:bg-red-700 font-bold">No</Button>
+              </div>
+            }
+            {/* <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" className="text-right">
+                  Name
+                </Label>
+                <Input id="name" value="Pedro Duarte" className="col-span-3" />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="username" className="text-right">
+                  Username
+                </Label>
+                <Input id="username" value="@peduarte" className="col-span-3" />
+              </div>
+            </div> */}
+            {/* <DialogFooter>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter> */}
+          </DialogContent>
+        </Dialog>
+      {/* </Link> */}
     </div>
   );
 };
