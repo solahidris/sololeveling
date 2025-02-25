dfafae8390d33f6121e070d59d905c1f79bc07e4
solahidris <emailsolah@gmail.com>
2025-02-26 01:24:02 +0800

    add seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="Level up like nobody's business with the anime inspired Solo Leveling daily routine. Level up the ranks and claim your throne." />
+        <meta name="keywords" content="solo, leveling, solo leveling, anime, solah, idris, solah idris" />
+        <meta name="author" content="Solah Idris" />
+        <meta property="og:title" content="Solo Leveling Fitness Tracker by Solah" />
+        <meta property="og:description" content="Level up like nobody's business with the anime inspired Solo Leveling daily routine. Level up the ranks and claim your throne." />
+        <meta property="og:url" content="https://sololevelingv0.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://sololevelingv0.vercel.app/images/noob.png" />
+      </Head>
       <body className="antialiased bg-black">
         <Main />
         <NextScript />
