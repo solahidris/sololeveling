7d507e54496e863718dab9b1f3793945f05ffe8e
solahidris <emailsolah@gmail.com>
2025-02-27 00:48:31 +0800

    add analytics pt2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { PlayerProvider } from "@/context/PlayerContext";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return  (
     <>
       <PlayerProvider>
         <Component {...pageProps} />
+        <Analytics />
       </PlayerProvider>
     </>
   )
